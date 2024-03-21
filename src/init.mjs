ba80f10c065427cf2cc92a215c15cef94c370235
ian612 <34078802+ian612@users.noreply.github.com>
2024-03-21 14:23:35 -0400

    Almost have it initializing properly

--- src/init.mjs
+++ src/init.mjs
@@ -1,3 +1,16 @@
-Hooks.once('ready', async () => {
-    console.warn("Hey dawg what's going on? Not much dudeee.");
-});
+import { preloadHandlebarsTemplates } from './module/templates.js';
+import { EnhancedItemSheetSFRPG } from './module/item/sheet.js'
+
+const MODULE_ID = 'sfrpg-item-sheets';
+
+
+
+Hooks.once('init', async () => {
+    console.warn(`${MODULE_ID} | Initializing Module`)
+    preloadHandlebarsTemplates()
+
+    DocumentSheetConfig.registerSheet(ItemSheet, MODULE_ID, EnhancedItemSheetSFRPG, {
+      label: 'SFRPGItemSheets.SheetLabel',
+      makeDefault: true
+    });
+  });
